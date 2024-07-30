6c9cb0b9a235beb8535b400ee01760b803705bb5
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-07-30 09:19:56 +0000

    User input value then compare both to see whcih is greater

---
+++ main.py
@@ -0,0 +1,9 @@
+#Take in two values from user
+val1 = input("Enter first value ")
+val2 = input("Enter second value ")
+
+#Check for the greatest value
+if val2> val1:
+  print(val2 ,"is greater")
+elif val1 >val2 :
+  print({val1} ,"is greater")
